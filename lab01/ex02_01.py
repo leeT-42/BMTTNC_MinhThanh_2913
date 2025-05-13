4fa7860b6dace43f45b9fb0cf76da4bc119ca35a
leeT-42 <leminhthanh931@gmail.com>
2025-05-13 16:21:16 +0700

    [add] lab01 ex02 ex03

--- lab01/ex02_01.py
+++
@@ -1,3 +0,0 @@
-ten = input("Nhập Tên: ")
-tuoi = input("Nhập Tuổi: ")
-print("Chào mừng", ten, "! Bạn", tuoi, "tuổi.")
--- lab01/ex02_06.py
+++
@@ -1,9 +0,0 @@
-input_str = input("Nhập x, y: ")
-dimensions=[int(x) for x in input_str.split(',')]
-rowNum= dimensions[0]
-colNum=dimensions[1]
-mutilist = [[0 for col in range(colNum)] for row in range(rowNum)]
-for row in range(rowNum):
-    for col in range(colNum):
-        mutilist[row][col]= row*col
-print(mutilist)
